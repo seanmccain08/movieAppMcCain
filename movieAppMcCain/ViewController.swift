9c7624b886edbb9e80899498517c931be3cd9770
seanmccain08 <seanmccain327@gmail.com>
2025-01-16 07:25:33 -0600

    Initial Commit

---
+++ movieAppMcCain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieAppMcCain
+//
+//  Created by SEAN MCCAIN on 1/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
